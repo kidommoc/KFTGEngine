f3a9bd99c63edd373d6a3ce0d352965713b3e57b
km <kidommoqi@outlook.com>
2019-07-19 11:49:47 +0800

    Supplement AsyncWrite

--- source/Engine/Asset/Filesystem.cpp
+++ source/Engine/Asset/Filesystem.cpp
@@ -56,7 +56,6 @@ void Filesystem::asyncRead (const string &path, void *buf, u32 size)
 {
 	HANDLE hFile = CreateFileA (path.getCStr (), GENERIC_READ, FILE_SHARE_READ,
 		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	// Async Read
 	AsyncIOInfo *info = (AsyncIOInfo*) _pool->alloc ();
 	info->path = &path;
 	info->buf = buf;
@@ -74,7 +73,17 @@ void Filesystem::asyncWrite (const string &path, void *content, u32 size)
 {
 	HANDLE hFile = CreateFileA (path.getCStr (), GENERIC_WRITE, FILE_SHARE_READ,
 		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	// Async Write
+	AsyncIOInfo *info = (AsyncIOInfo*) _pool->alloc ();
+	info->path = &path;
+	info->buf = content;
+	info->size = size;
+	info->fs = this;
+	info->index = hash (path) / ASYNC_TABLE_SIZE;
+	info->finish = finishAsyncIO;
+	DWORD threadId;
+	HANDLE hThread = CreateThread (NULL, 0, (LPTHREAD_START_ROUTINE) writeThread,
+		info, 0, &threadId);
+	SetThreadPriority (hThread, THREAD_PRIORITY_BELOW_NORMAL);
 }
 
 bool Filesystem::queryAsyncIO (const string &path)
